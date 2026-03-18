75608d9d0bd87079217d442ec99b23295744aef9
Pavanntej <pavanntej@gmail.com>
2026-03-18 11:36:28 +0530

    Update main.jsx

--- public-site/src/main.jsx
+++ public-site/src/main.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Book from "./pages/Book"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/book/:id" element={<Book />} />
+    </Routes>
+  </BrowserRouter>
 )
